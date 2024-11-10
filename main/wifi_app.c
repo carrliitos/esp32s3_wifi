61fc39104c6be0005921564655e98b608d203c93
carrliitos <salazar.ten80@gmail.com>
2024-11-09 18:21:05 -0600

    Enhance WiFi application with event handling, default configuration, and SoftAP setup
    - Added `wifi_app_event_handler` to handle WiFi and IP events.
    - Implemented `wifi_app_default_wifi_init` to initialize TCP stack and default WiFi configuration.
    - Created `wifi_app_soft_ap_config` to configure SoftAP settings, including static IP assignment and DHCP server setup.
    - Registered WiFi and IP event handlers through `wifi_app_event_handler_init`.
    - Refactored main WiFi task to incorporate event handling and initial configuration steps.

--- main/wifi_app.c
+++ main/wifi_app.c
@@ -12,21 +12,126 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
-#include "lwip_netdb.h"
+#include "lwip/netdb.h"
 
 #include "rgb_led.h"
-#include "task_common.h"
+#include "tasks_common.h"
 #include "wifi_app.h"
 
 // Tag used for ESP serial console messages
 static const char TAG [] = "wifi_app";
 
 // Queue handle used to manipulate the main queue of events
-status QueueHandle_t wifi_app_queue_handle;
+static QueueHandle_t wifi_app_queue_handle;
 
 // netif objects for the station and access point
 esp_netif_t* esp_netif_sta = NULL;
 esp_netif_t* esp_netif_ap  = NULL;
+
+/**
+ * WiFi application event handler
+ * @param arg data, aside from event data, that is passed to the handler when it is called.
+ * @param event_base the base id of the event to register the handler for.
+ * @param event_id the id for the event to register the handler for.
+ * @param event_data is the event data.
+ */
+static void wifi_app_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT) {
+    switch(event_id) {
+      case WIFI_EVENT_AP_START:
+        ESP_LOGI(TAG, "WIFI_EVENT_AP_START");
+        break;
+      case WIFI_EVENT_AP_STOP:
+        ESP_LOGI(TAG, "WIFI_EVENT_AP_START");
+        break;
+      case WIFI_EVENT_AP_STACONNECTED:
+        ESP_LOGI(TAG, "WIFI_EVENT_AP_STACONNECTED");
+        break;
+      case WIFI_EVENT_AP_STADISCONNECTED:
+        ESP_LOGI(TAG, "WIFI_EVENT_AP_STADISCONNECTED");
+        break;
+      case WIFI_EVENT_STA_START:
+        ESP_LOGI(TAG, "WIFI_EVENT_STA_START");
+        break;
+      case WIFI_EVENT_STA_CONNECTED:
+        ESP_LOGI(TAG, "WIFI_EVENT_STA_CONNECTED");
+        break;
+      case WIFI_EVENT_STA_DISCONNECTED:
+        ESP_LOGI(TAG, "WIFI_EVENT_STA_DISCONNECTED");
+        break;
+    }
+  }
+  else if (event_base == IP_EVENT) {
+    switch(event_id) {
+      case IP_EVENT_STA_GOT_IP:
+        ESP_LOGI(TAG, "IP_EVENT_STA_GOT_IP");
+        break;
+    }
+  }
+}
+
+/**
+ * Initializes the TCP stack and default WiFi configuration.
+ */
+static void wifi_app_default_wifi_init(void) {
+  // Initialize the TCP stack
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  // Default WiFi config -- operations must be in this order
+  wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  esp_netif_sta = esp_netif_create_default_wifi_sta();
+  esp_netif_ap = esp_netif_create_default_wifi_ap();
+}
+
+/**
+ * Configures the WiFi AP setttings and assigns the static IP to the SoftAP.
+ */
+static void wifi_app_soft_ap_config(void) {
+  // SoftAP - WiFi Access Point configuration
+  wifi_config_t ap_config = {
+    .ap = {
+      .ssid            = WIFI_AP_SSID,
+      .ssid_len        = strlen(WIFI_AP_SSID),
+      .password        = WIFI_AP_PASSWORD,
+      .channel         = WIFI_AP_CHANNEL,
+      .ssid_hidden     = WIFI_AP_SSID_HIDDEN,
+      .authmode        = WIFI_AUTH_WPA2_PSK,
+      .max_connection  = WIFI_AP_MAX_CONNECTIONS,
+      .beacon_interval = WIFI_AP_BEACON_INTERVAL,
+    },
+  };
+  // Configure DHCP for the AP
+  esp_netif_ip_info_t ap_ip_info;
+  memset(&ap_ip_info, 0x00, sizeof(ap_ip_info));                              // we want this cleared out first
+
+  esp_netif_dhcps_stop(esp_netif_ap);                                         // must call this first!
+  inet_pton(AF_INET, WIFI_AP_IP, &ap_ip_info.ip);                             // assigns access point's staic IP, gateway, and netmask
+  inet_pton(AF_INET, WIFI_AP_GATEWAY, &ap_ip_info.gw);
+  inet_pton(AF_INET, WIFI_AP_NETMASK, &ap_ip_info.netmask);
+  ESP_ERROR_CHECK(esp_netif_set_ip_info(esp_netif_ap, &ap_ip_info));          // Statically configures the network interface 
+  ESP_ERROR_CHECK(esp_netif_dhcps_start(esp_netif_ap));                       // Starts the AP DHCP server (for connecting stations, e.g., your mobile device)
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));                        // Setting the mode as access point/station mode
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));           // Set our configuration
+  ESP_ERROR_CHECK(esp_wifi_set_bandwidth(ESP_IF_WIFI_AP, WIFI_AP_BANDWIDTH)); // Default bandwidth 20MHz
+  ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_STA_POWER_SAVE));                      // Power save set to NONE
+}
+
+/**
+ * Initializes the WiFi application handler for WiFi and IP events
+ */
+static void wifi_app_event_handler_init(void) {
+  // Event loop for the WiFi driver
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  // Create the event handler for the connection
+  esp_event_handler_instance_t instance_wifi_event;
+  esp_event_handler_instance_t instance_ip_event;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_app_event_handler, NULL, &instance_wifi_event));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &wifi_app_event_handler, NULL, &instance_ip_event));
+}
 
 /**
  * Main task for the WiFi application
@@ -35,20 +140,11 @@ esp_netif_t* esp_netif_ap  = NULL;
 static void wifi_app_task(void *pvParameters) {
   wifi_app_queue_message_t msg;
 
-  // initialize the event handler
-  wifi_app_event_handler_init();
-
-  // Initialize the TCP/IP stack and WiFi config
-  wifi_app_default_wifi_init();
-
-  // SoftAP config
-  wifi_app_soft_ap_config();
-
-  // Start WiFi
-  ESP_ERROR_CHECK(esp_wifi_start());
-
-  // Send the first event message
-  wifi_app_send_message(WIFI_APP_MSG_START_HTTP_SERVER);
+  wifi_app_event_handler_init();                         // initialize the event handler
+  wifi_app_default_wifi_init();                          // Initialize the TCP/IP stack and WiFi config
+  wifi_app_soft_ap_config();                             // SoftAP config
+  ESP_ERROR_CHECK(esp_wifi_start());                     // Start WiFi
+  wifi_app_send_message(WIFI_APP_MSG_START_HTTP_SERVER); // Send the first event message
 
   for(;;) {
     if (xQueueReceive(wifi_app_queue_handle, &msg, portMAX_DELAY)) {
@@ -63,7 +159,7 @@ static void wifi_app_task(void *pvParameters) {
           ESP_LOGI(TAG, "WIFI_APP_MSG_CONNECT_FROM_HTTP_SERVER");
           break;
         case WIFI_APP_MSG_STA_CONNECTED_GOT_IP:
-          ESP_LOGI(TAG, WIFI_APP_MSG_STA_CONNECTED_GOT_IP);
+          ESP_LOGI(TAG, "WIFI_APP_MSG_STA_CONNECTED_GOT_IP");
           rgb_led_wifi_connected();
           break;
         default:
@@ -82,14 +178,9 @@ BaseType_t wifi_app_send_message(wifi_app_message_e msgID) {
 void wifi_app_start(void) {
   ESP_LOGI(TAG, "Starting WiFi Application");
 
-  // Start WiFi started LED
-  rgb_led_wifi_app_started();
-
-  // Disable default WiFi logging messages
-  esp_log_level_set("wifi", ESP_LOG_NONE);
-
-  // Create message queue
-  wifi_app_queue_handle = xQueueCreate(3, sizeof(wifi_app_queue_message_t));
+  rgb_led_wifi_app_started();                                                // Start WiFi started LED
+  esp_log_level_set("wifi", ESP_LOG_NONE);                                   // Disable default WiFi logging messages
+  wifi_app_queue_handle = xQueueCreate(3, sizeof(wifi_app_queue_message_t)); // Create message queue
 
   // Start the WiFi application task
   xTaskCreatePinnedToCore(&wifi_app_task, "wifi_app_task", WIFI_APP_TASK_STACK_SIZE, NULL, WIFI_APP_TASK_PRIORITY, NULL, WIFI_APP_TASK_CORE_ID);
