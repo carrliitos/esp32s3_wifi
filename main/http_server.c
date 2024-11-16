f44fd8dcf46026f5b34dcf770a1e4917907b672d
carrliitos <salazar.ten80@gmail.com>
2024-11-16 11:36:18 -0600

    initialize HTTP Server files

---
+++ main/http_server.c
@@ -0,0 +1,9 @@
+/*
+ * http_server.c
+ *
+ * Created on:         2024-11-16 11:17:52
+ *     Author:         carrliitos (benzon.salazar@gmail.com)
+ *
+ * Last Modified by:   carrliitos
+ * Last Modified time: 2024-11-16 11:34:21
+ */
---
+++ main/http_server.h
@@ -0,0 +1,15 @@
+/*
+ * http_server.h
+ *
+ * Created on:         2024-11-16 11:34:11
+ *     Author:         carrliitos (benzon.salazar@gmail.com)
+ *
+ * Last Modified by:   carrliitos
+ * Last Modified time: 2024-11-16 11:35:53
+ */
+
+#ifndef MAIN_HTTP_SERVER_H
+#define MAIN_HTTP_SERVER_H
+
+
+#endif /* MAIN_HTTP_SERVER_H */
